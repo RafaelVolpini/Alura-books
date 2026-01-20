6c131b1e32a17ea43b87ca513e7d4cc25364e774
RafaelVolp <rafael.volpini@tsaengenharia.com>
2026-01-20 13:20:04 -0300

    Componente de Input

---
+++ vite-project/src/components/Search/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+export default function Search () {
+    return (
+        <>
+            <h2>Já sabe por onde começar?</h2>
+            <h3>Encontre seu livro em nossa estante.</h3>
+            <input type="text" 
+            placeholder="Escreva sua próxima leitura..."/>
+        </>
+    )
+}
